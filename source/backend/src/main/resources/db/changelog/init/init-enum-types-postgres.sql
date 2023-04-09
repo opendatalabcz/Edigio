01594b4c8164e11abfca00ec959b1340954ff096
Filip Ballek <filip.ballek@profinit.eu>
2023-04-09 16:58:42 +0200

    Used database domain instead of enum because of problems when used together with JPA

--- source/backend/src/main/resources/db/changelog/init/init-enum-types-postgres.sql
+++ source/backend/src/main/resources/db/changelog/init/init-enum-types-postgres.sql
@@ -1,64 +1,56 @@
-CREATE TYPE project_status AS ENUM (
-    'PREPARED',
-    'PUBLISHED',
-    'ARCHIVED'
-);
+CREATE DOMAIN project_status AS varchar
+    CHECK ( VALUE IN ('PREPARED'::varchar,
+                      'PUBLISHED'::varchar,
+                      'ARCHIVED'::varchar));
 
-CREATE TYPE catastrophe_type AS ENUM (
-    'DROUGHT',
-    'HIGH_TEMPERATURES',
-    'WIND',
-    'HEAVY_RAINFALL',
-    'FLOODING',
-    'FOOD_AND_WATER_OUTAGE',
-    'BIOTIC_EMERGENCY',
-    'CHEMICAL_EMERGENCY',
-    'ENERGY_OUTAGE',
-    'MIGRATION',
-    'CRIMINALITY',
-    'OTHER'
-);
+CREATE DOMAIN catastrophe_type AS varchar
+    CHECK ( VALUE IN ('DROUGHT',
+                      'HIGH_TEMPERATURES',
+                      'WIND',
+                      'HEAVY_RAINFALL',
+                      'FLOODING',
+                      'FOOD_AND_WATER_OUTAGE',
+                      'BIOTIC_EMERGENCY',
+                      'CHEMICAL_EMERGENCY',
+                      'ENERGY_OUTAGE',
+                      'MIGRATION',
+                      'CRIMINALITY',
+                      'OTHER' ));
 
-CREATE TYPE advertisement_status AS ENUM (
-    'CREATED',
-    'PUBLISHED',
-    'EDITED',
-    'CANCELED',
-    'RESOLVED'
-);
+CREATE DOMAIN advertisement_status AS varchar
+    CHECK ( VALUE IN ('CREATED',
+                      'PUBLISHED',
+                      'EDITED',
+                      'CANCELED',
+                      'RESOLVED'));
 
-CREATE TYPE advertisement_help_type AS ENUM (
-    'ACCOMMODATIONS',
-    'MATERIAL',
-    'FOOD_AND_WATER',
-    'EMERGENCY_RESPONSE_EQUIPMENT',
-    'MEDICAL_SUPPLIES',
-    'VOLUNTEERING',
-    'SPECIALIST',
-    'CRAFTSMAN',
-    'RIDE',
-    'MEDICAL_ASSISTANCE',
-    'PSYCHOLOGICAL_HELP',
-    'ADMINISTRATION',
-    'OTHER'
-);
+CREATE DOMAIN advertisement_help_type AS varchar
+    CHECK ( VALUE IN ('ACCOMMODATIONS',
+                      'MATERIAL',
+                      'FOOD_AND_WATER',
+                      'EMERGENCY_RESPONSE_EQUIPMENT',
+                      'MEDICAL_SUPPLIES',
+                      'VOLUNTEERING',
+                      'SPECIALIST',
+                      'CRAFTSMAN',
+                      'RIDE',
+                      'MEDICAL_ASSISTANCE',
+                      'PSYCHOLOGICAL_HELP',
+                      'ADMINISTRATION',
+                      'OTHER'));
 
-CREATE TYPE advertisement_type AS ENUM (
-    'OFFER',
-    'REQUEST'
-);
+CREATE DOMAIN advertisement_type AS varchar
+    CHECK ( VALUE IN ('OFFER', 'REQUEST') );
 
-CREATE TYPE response_status AS ENUM (
-    'WAITING_FOR_CONTACT_CONFIRMATION',
-    'WAITING_FOR_RESOLVE',
-    'REJECTED',
-    'ACCEPTED',
-    'REJECTED_ON_ADVERTISEMENT_RESOLVE'
-);
+CREATE DOMAIN response_status AS varchar
+    CHECK ( VALUE IN ('WAITING_FOR_CONTACT_CONFIRMATION',
+                      'WAITING_FOR_RESOLVE',
+                      'REJECTED',
+                      'ACCEPTED',
+                      'REJECTED_ON_ADVERTISEMENT_RESOLVE'));
 
-CREATE TYPE user_role AS ENUM (
-    'ANONYMOUS_USER',
-    'USER',
-    'COORDINATOR',
-    'ADMIN'
-);
+CREATE DOMAIN user_role AS varchar
+    CHECK ( VALUE IN ('ANONYMOUS_USER',
+                      'USER',
+                      'COORDINATOR',
+                      'ADMIN'));
--- source/backend/src/main/resources/db/changelog/init/init-use-enum-types-postgres.sql
+++ source/backend/src/main/resources/db/changelog/init/init-use-enum-types-postgres.sql
@@ -1,23 +1,23 @@
 ALTER TABLE project
-    ALTER COLUMN status TYPE project_status,
-    ALTER COLUMN catastrophe_type TYPE catastrophe_type;
+    ALTER COLUMN status TYPE project_status USING status::project_status,
+    ALTER COLUMN catastrophe_type TYPE catastrophe_type USING catastrophe_type::catastrophe_type;
 
 ALTER TABLE advertisement
-    ALTER COLUMN type TYPE advertisement_type,
-    ALTER COLUMN status TYPE advertisement_status,
-    ALTER COLUMN help_type TYPE advertisement_help_type;
+    ALTER COLUMN type TYPE advertisement_type USING type::advertisement_type,
+    ALTER COLUMN status TYPE advertisement_status USING status::advertisement_status,
+    ALTER COLUMN help_type TYPE advertisement_help_type USING help_type::advertisement_help_type;
 
 ALTER TABLE advertisement_response
-    ALTER COLUMN response_status TYPE response_status;
+    ALTER COLUMN response_status TYPE response_status USING response_status::response_status;
 
 ALTER TABLE advertisement_template_catastrophe_types
-    ALTER COLUMN catastrophe_type TYPE catastrophe_type;
+    ALTER COLUMN catastrophe_type TYPE catastrophe_type USING catastrophe_type::catastrophe_type;
 
 ALTER TABLE advertisement_template_advertisement_help_types
-    ALTER COLUMN help_type TYPE advertisement_help_type;
+    ALTER COLUMN help_type TYPE advertisement_help_type USING help_type::advertisement_help_type;
 
 ALTER TABLE advertisement_template_advertisement_types
-    ALTER COLUMN advertisement_type TYPE advertisement_type;
+    ALTER COLUMN advertisement_type TYPE advertisement_type USING advertisement_type::advertisement_type;
 
 ALTER TABLE user_account
-    ALTER COLUMN role TYPE user_role;
+    ALTER COLUMN role TYPE user_role USING role::user_role;
