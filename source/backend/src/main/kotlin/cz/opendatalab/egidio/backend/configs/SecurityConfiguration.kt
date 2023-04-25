e623852ddfba81ff9c1cdb2a4713f51f3e8416b9
Filip Ballek <filip.ballek@profinit.eu>
2023-04-25 13:57:31 +0200

    CSRF token disabled, added fav icon, minor tweaks

--- source/backend/src/main/kotlin/cz/opendatalab/egidio/backend/configs/SecurityConfiguration.kt
+++ source/backend/src/main/kotlin/cz/opendatalab/egidio/backend/configs/SecurityConfiguration.kt
@@ -18,7 +18,6 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 import org.springframework.security.web.savedrequest.NullRequestCache
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -51,8 +50,7 @@ class SecurityConfiguration : WebMvcConfigurer {
             .and()
             .cors()
             .and()
-            .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-            .and()
+            .csrf().disable()
             .httpBasic()
         return http.build()
     }
