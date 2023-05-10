d9016337573019725e7548a0f74163c4ccde95c3
Filip Ballek <filip.ballek@profinit.eu>
2023-05-10 20:23:36 +0200

    Added missing permissions check to important information service

--- source/backend/src/main/kotlin/cz/opendatalab/egidio/backend/business/services/important_information/ImportantInformationService.kt
+++ source/backend/src/main/kotlin/cz/opendatalab/egidio/backend/business/services/important_information/ImportantInformationService.kt
@@ -1,9 +1,13 @@
 package cz.opendatalab.egidio.backend.business.services.important_information
 
+import cz.opendatalab.egidio.backend.business.authentication.annotations.PermitCoordinator
 import cz.opendatalab.egidio.backend.business.entities.important_information.ImportantInformation
 import cz.opendatalab.egidio.backend.presentation.dto.important_information.ImportantInformationCreateDto
+import jakarta.annotation.security.PermitAll
 
 interface ImportantInformationService {
+    @PermitCoordinator
     fun create(importantInformation: ImportantInformationCreateDto) : ImportantInformation
+    @PermitAll
     fun getAllBySlugs(slugs: List<String>): List<ImportantInformation>
 }
